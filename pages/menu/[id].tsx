1b29b55d53bca551ba52400effdbe073fd54ebbe
jongvnnar <jgh060@gmail.com>
2022-04-10 21:48:36 +0000

    basic product

--- pages/menu/[id].tsx
+++ pages/menu/[id].tsx
@@ -1,17 +1,69 @@
 import Head from 'next/head';
-import { NextPage } from 'next/types';
+import { GetServerSideProps, NextPage } from 'next/types';
+import { MenuItem } from '../../types/Menu';
+import Image from 'next/image';
+import styles from '../../styles/Product.module.scss';
+import { formatPrice } from '../../lib/price-format';
+import { formatDateString } from '../../lib/date-ops';
 
-const Product: NextPage = () => {
+type Props = {
+  product: MenuItem;
+};
+const Product: NextPage<Props> = ({ product }: Props) => {
   return (
-    <>
+    <div className={styles.container}>
       <Head>
-        <title>Vara</title>
+        <title>{product.title}</title>
         <meta name="description" content="Upplýsingar um vöru" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>Vara</div>
-    </>
+      <div className={styles.heading_container}>
+        <h1>{product.title}</h1>
+      </div>
+      <div className={styles.image_container}>
+        <Image
+          alt={`Image of ${product.title}`}
+          src={product.image}
+          layout="fill"
+          objectFit="contain"
+          objectPosition={'left center'}
+        />
+      </div>
+      <div className={styles.details_container}>
+        <h2>Lýsing</h2>
+        <p>{product.description}</p>
+        <h2>Verð</h2>
+        <p>{formatPrice(product.price)}</p>
+        {/* TODO category names */}
+        <h2>Tegund</h2>
+        <p>{product.category}</p>
+        <h2>Búið til</h2>
+        <p>{formatDateString(product.created)}</p>
+        <h2>Síðast breytt</h2>
+        <p>{formatDateString(product.updated)}</p>
+      </div>
+    </div>
   );
 };
 
+export const getServerSideProps: GetServerSideProps<Props> = async (
+  context
+) => {
+  const { id = '' } = context.params || {};
+  if (!id || typeof id !== 'string') {
+    return { notFound: true };
+  }
+  const url = new URL(`/menu/${id}`, process.env.NEXT_PUBLIC_API_URL);
+  const response = await fetch(url.toString());
+
+  // TODO finna út úr error handling
+  if (!response.ok) {
+    return {
+      notFound: true,
+    };
+  }
+
+  const product: MenuItem = await response.json();
+  return { props: { product } };
+};
 export default Product;
---
+++ types/Menu.ts
@@ -0,0 +1,10 @@
+export type MenuItem = {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  image: string;
+  category: number;
+  created: string;
+  updated: string;
+};
