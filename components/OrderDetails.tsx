41f86c4074c575282dbcd2a74f9519497c4e01c6
jongvnnar <jgh060@gmail.com>
2022-04-10 18:19:48 +0000

    order details

---
+++ components/OrderDetails.tsx
@@ -0,0 +1,54 @@
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import { OrderLine } from '../types/order';
+import styles from '../styles/OrderDetails.module.scss';
+
+type Props = {
+  lines: OrderLine[];
+};
+export function OrderDetails({ lines }: Props) {
+  const [sum, setSum] = useState<number>(
+    lines.reduce((agg, line) => agg + line.total, 0)
+  );
+
+  useEffect(() => {
+    setSum(lines.reduce((agg, line) => agg + line.total, 0));
+  }, [lines]);
+
+  return (
+    <div className={styles.table_container}>
+      <table>
+        <thead>
+          <tr>
+            <th>Vara</th>
+            <th className={styles.number_cell}>Verð</th>
+            <th className={styles.number_cell}>Fjöldi</th>
+            <th className={styles.number_cell}>Heildarverð</th>
+          </tr>
+        </thead>
+        <tbody>
+          {lines.map((value) => {
+            return (
+              <tr key={value.productId}>
+                <td>
+                  <Link href={`products/${value.productId}`}>
+                    <a className={styles.link}>{value.title}</a>
+                  </Link>
+                </td>
+                <td className={styles.number_cell}>{value.price}</td>
+                <td className={styles.number_cell}>{value.quantity}</td>
+                <td className={styles.number_cell}>{value.total}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+        <tfoot>
+          <tr>
+            <th colSpan={3}>Heildarsumma</th>
+            <td className={styles.number_cell}>{sum}</td>
+          </tr>
+        </tfoot>
+      </table>
+    </div>
+  );
+}
--- pages/orders/[id].tsx
+++ pages/orders/[id].tsx
@@ -1,17 +1,19 @@
-import type { GetServerSideProps, GetStaticPaths, NextPage } from "next";
-import Head from "next/head";
-import { Error } from "../../types/Error";
-import { Order } from "../../types/order";
-import styles from "../../styles/Order.module.scss";
-import { useRouter } from "next/router";
-import { WatchStatus } from "../../components/WatchStatus";
-
+import type { GetServerSideProps, GetStaticPaths, NextPage } from 'next';
+import Head from 'next/head';
+import { Error } from '../../types/Error';
+import { Order } from '../../types/order';
+import styles from '../../styles/Order.module.scss';
+import { useRouter } from 'next/router';
+import { WatchStatus } from '../../components/WatchStatus';
+import { formatDateString } from '../../lib/date-ops';
+import { OrderDetails } from '../../components/OrderDetails';
 type Props = {
   order: Order;
 };
 const Order: NextPage<Props> = ({ order }) => {
   const router = useRouter();
-  const { id = "" } = router.query;
+  const { id = '' } = router.query;
+
   return (
     <div className={styles.container}>
       <Head>
@@ -19,11 +21,18 @@ const Order: NextPage<Props> = ({ order }) => {
         <meta name="description" content="Upplýsingar um pöntun" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className={styles.heading_container}></div>
+      <div className={styles.heading_container}>
+        <h1>Pöntun</h1>
+        <h3>{order.name}</h3>
+        <h3>Móttekið dags {formatDateString(order.created)}</h3>
+      </div>
       <div className={styles.state_container}>
         <WatchStatus id={id} initialStates={order.status} />
       </div>
-      <div className={styles.details_container}></div>
+      <div className={styles.details_container}>
+        <h2>Upplýsingar um pöntun</h2>
+        <OrderDetails lines={order.lines} />
+      </div>
     </div>
   );
 };
@@ -31,8 +40,8 @@ const Order: NextPage<Props> = ({ order }) => {
 export const getServerSideProps: GetServerSideProps<Props> = async (
   context
 ) => {
-  const { id = "" } = context.params || {};
-  if (!id || typeof id !== "string") {
+  const { id = '' } = context.params || {};
+  if (!id || typeof id !== 'string') {
     return { notFound: true };
   }
   const url = new URL(`/orders/${id}`, process.env.NEXT_PUBLIC_API_URL);
