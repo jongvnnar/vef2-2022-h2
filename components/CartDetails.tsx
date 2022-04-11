794db56440e75e9b91458a748e48a3c738d8cb52
Fannar Steinn <fannarsteinn@hotmail.com>
2022-04-11 00:02:05 +0000

    place order

--- components/CartDetails.tsx
+++ components/CartDetails.tsx
@@ -5,14 +5,29 @@ import { formatPrice } from '../lib/price-format';
 import Button from './Button';
 import { useCart } from '../context/CartContext';
 import { useEffect, useState } from 'react';
+import classNames from 'classnames';
+import { useRouter } from 'next/router';
+
+const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
 type Props = {
   cart: Cart;
 };
 
+type Error = {
+  location: string;
+  msg: string;
+  param: string;
+  value: string;
+};
+
 export default function CartDetails({ cart }: Props) {
   const { deleteCart } = useCart();
 
+  const router = useRouter();
+
+  const [name, setName] = useState('');
+  const [isNameError, setNameError] = useState(false);
   const [sum, setSum] = useState<number>(
     cart.lines.reduce((agg, line) => agg + line.total, 0)
   );
@@ -20,6 +35,31 @@ export default function CartDetails({ cart }: Props) {
   useEffect(() => {
     setSum(cart.lines.reduce((agg, line) => agg + line.total, 0));
   }, [cart]);
+
+  const placeOrder = async () => {
+    setNameError(false);
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        cart: cart.id,
+        name,
+      }),
+    };
+    const result = await fetch(`${apiUrl}/orders`, options);
+    const response = await result.json();
+
+    if (result.ok) {
+      router.push(`/orders/${response.id}`);
+      deleteCart();
+    } else if (result.status === 400) {
+      if (response?.errors?.some((e: Error) => e.param === 'name')) {
+        setNameError(true);
+      }
+    }
+  };
 
   return (
     <>
@@ -40,11 +80,28 @@ export default function CartDetails({ cart }: Props) {
           <CartLineDetails key={item.id} line={item} />
         ))}
       <p className={s.cartTotal}>Order total price: {formatPrice(sum)}</p>
+      <div className={s.orderName}>
+        <label htmlFor="order-name">Enter name for order:</label>
+        <div>
+          <input
+            id="order-name"
+            type="text"
+            value={name}
+            className={classNames({ [s.error]: isNameError })}
+            onChange={(e) => setName(e.target.value)}
+          ></input>
+          {isNameError ? (
+            <p className={s.errorLabel}>Name is required</p>
+          ) : (
+            <></>
+          )}
+        </div>
+      </div>
       <div className={s.mainActions}>
         <Button size="large" primary={false} onClick={deleteCart}>
           Clear cart
         </Button>
-        <Button size="large" primary={true}>
+        <Button size="large" primary={true} onClick={placeOrder}>
           Place order
         </Button>
       </div>
