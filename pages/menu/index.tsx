ccfdc8cabd5405f196e612fa3d9036109bb0b549
jongvnnar <jgh060@gmail.com>
2022-04-19 15:05:52 +0000

    start categories

--- pages/menu/index.tsx
+++ pages/menu/index.tsx
@@ -1,7 +1,15 @@
-import type { NextPage } from 'next';
+import type { GetServerSideProps, NextPage } from 'next';
 import Head from 'next/head';
+import Link from 'next/link';
+import { Category } from '../../types/Category';
+import { MenuItem } from '../../types/Menu';
+import { PagedResponse } from '../../types/PagedResponse';
 
-const Menu: NextPage = () => {
+type Props = {
+  menu: PagedResponse<MenuItem>;
+  categories: PagedResponse<Category>;
+};
+const Menu: NextPage<Props> = ({ categories }) => {
   return (
     <div>
       <Head>
@@ -9,9 +17,32 @@ const Menu: NextPage = () => {
         <meta name="description" content="The restaurant's menu!" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>Menu</div>
+      {/** Dæmi um að skipta um categories */}
+      {categories.items.map((value) => {
+        return (
+          <li key={value.id}>
+            <Link href={`?category=${value.id}`}>
+              <a>{value.title}</a>
+            </Link>
+          </li>
+        );
+      })}
     </div>
   );
+};
+
+export const getServerSideProps: GetServerSideProps<Props> = async (
+  context
+) => {
+  const menuUrl = new URL(context.resolvedUrl, process.env.NEXT_PUBLIC_API_URL);
+  const menuResponse = await fetch(menuUrl.toString());
+  const categoryUrl = new URL('categories', process.env.NEXT_PUBLIC_API_URL);
+  const categoryResponse = await fetch(categoryUrl.toString());
+  const categories: PagedResponse<Category> = await categoryResponse.json();
+  const menu: PagedResponse<MenuItem> = await menuResponse.json();
+  return {
+    props: { menu, categories },
+  };
 };
 
 export default Menu;
