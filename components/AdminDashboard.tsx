85e75f51bb860d655e70313156da6298f41fb619
Fannar Steinn <fannarsteinn@hotmail.com>
2022-04-20 23:43:57 +0000

    manage categories

--- components/AdminDashboard.tsx
+++ components/AdminDashboard.tsx
@@ -1,5 +1,6 @@
 import { useAuth } from "../context/Auth";
 import Button from "./Button";
+import CategoriesManager from "./CategoriesManager";
 
 export default function AdminDashboard() {
   const { user, logoutUser } = useAuth();
@@ -12,6 +13,7 @@ export default function AdminDashboard() {
   return (
     <div>
       <h1>Administration Dashboard</h1>
+      <CategoriesManager />
       <Button type="button" size="large" onClick={logout} primary={false} >
         Log out
       </Button>
---
+++ components/CategoriesManager.tsx
@@ -0,0 +1,190 @@
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { useAuth } from "../context/Auth";
+import { Category } from "../types/Category";
+import { Input } from "./Input";
+
+const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+
+export default function CategoriesManager() {
+  const { token } = useAuth();
+
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+  const [categories, setCategories] = useState([]);
+
+  const [newTitle, setNewTitle] = useState('');
+
+  async function fetchData() {
+    setLoading(true);
+    setError('');
+
+    let json;
+
+    try {
+      const result = await fetch(`${apiUrl}/categories`);
+
+      if (!result.ok) {
+        throw new Error('Results not ok');
+      }
+
+      json = await result.json();
+    } catch (e) {
+      console.warn('unable to fetch categories', e);
+      setError('Unable to fetch categories');
+      return;
+    } finally {
+      setLoading(false);
+    }
+
+    setCategories(json?.items || []);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const saveNewCategory = async () => {
+    try {
+      const options = {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json',
+          Authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify({
+          title: newTitle
+        })
+      };
+      const result = await fetch(`${apiUrl}/categories`, options);
+
+      if (!result.ok) {
+        console.error('Category not created');
+      } else {
+        setNewTitle('');
+        fetchData();
+      }
+
+    } catch (e) {
+      console.warn('unable to create category', e);
+      return;
+    }
+  }
+
+  if (loading) {
+    return <p>loading...</p>
+  }
+
+  return(
+    <div>
+      <h2>Categories</h2>
+      <ul>
+      {categories.length !== 0 ? (
+        <ul>
+          {categories.map((item: Category) => (
+            <li key={item.id}>
+              <SingleCategoryEditor category={item} refresh={fetchData} />
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>Engir flokkar</p>
+      )}
+      </ul>
+      <div>
+        <p>Create a new category:</p>
+        <Input label="" name="" value={newTitle} setValue={setNewTitle} />
+        <button onClick={saveNewCategory}>
+          <Image src="/save_icon.svg" width={20} height={20} alt="" />
+        </button>
+      </div>
+    </div>
+  );
+}
+
+function SingleCategoryEditor({ category, refresh } : { category: Category, refresh: () => void }) {
+  const { token } = useAuth();
+
+  const [editMode, setEditMode] = useState(false);
+  const [title, setTitle] = useState(category.title);
+
+  const edit = () => {
+    setEditMode(!editMode);
+  }
+
+  const update = async (id: number) => {
+    try {
+      const options = {
+        method: 'PATCH',
+        headers: {
+          'content-type': 'application/json',
+          Authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify({
+          title
+        })
+      };
+      const result = await fetch(`${apiUrl}/categories/${id}`, options);
+
+      if (!result.ok) {
+        console.error('Category not created');
+      } else {
+        setTitle('')
+        edit();
+        refresh();
+      }
+
+    } catch (e) {
+      console.warn('unable to create category', e);
+      return;
+    }
+  }
+
+  const deleteCategory = async (id: number) => {
+    try {
+      const options = {
+        method: 'DELETE',
+        headers: {
+          Authorization: `Bearer ${token}`
+        },
+      };
+      const result = await fetch(`${apiUrl}/categories/${id}`, options);
+
+      if (!result.ok) {
+        console.error('Category not deleted');
+      } else {
+        refresh();
+      }
+
+    } catch (e) {
+      console.warn('unable to delete category', e);
+      return;
+    }
+  }
+
+  if (editMode) {
+    return (
+      <div>
+        <Input label="" name="" value={title} setValue={setTitle} />
+        <button onClick={() => update(category.id)}>
+          <Image src="/save_icon.svg" width={20} height={20} alt="" />
+        </button>
+        <button onClick={edit}>
+          <Image src="/close_icon.svg" width={20} height={20} alt="" />
+        </button>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <p>{category.title}</p>
+      <button onClick={edit}>
+        <Image src="/edit_icon.svg" width={20} height={20} alt="" />
+      </button>
+      <button onClick={() => deleteCategory(category.id)}>
+        <Image src="/close_icon.svg" width={20} height={20} alt="" />
+      </button>
+    </div>
+  );
+}
