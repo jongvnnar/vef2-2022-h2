0312767fcc344cdf307828fd3dfab52ea8df7260
Ben-Ivars <60788806+Ben-Ivars@users.noreply.github.com>
2022-04-22 22:52:20 +0000

    ekkert vesen

--- cypress/integration/tests/menu.spec.js
+++ cypress/integration/tests/menu.spec.js
@@ -2,7 +2,7 @@ describe('Menu', () => {
   it('should show menu item', () => {
     cy.visit('/menu');
     cy.get('.MenuItemCard_card__kdijn').should('be.visible');
-    cy.get('.Menu_container__6cT12 :nth-child(1)').click();
+    cy.get(':nth-child(1) > .MenuItemCard_card__kdijn > a > :nth-child(1) > [style="box-sizing:border-box;display:inline-block;overflow:hidden;width:initial;height:initial;background:none;opacity:1;border:0;margin:0;padding:0;position:relative;max-width:100%"] > .MenuItemCard_image__yOcr5').click();
     cy.url().should('include', '/menu/');
   });
 });
